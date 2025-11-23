c3f90db7dd1ceda1074f65e0036d6c8b748a3af6
am499702@gmail.com <am499702@gmail.com>
2025-11-23 18:14:36 +0530

    first commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using onboardingAPI.Data;
+using onboardingAPI.DTOs;
+
+
+namespace onboardingAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController:ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var user=await _context.Users.FirstOrDefaultAsync(x=>x.Email==request.Email); // Retreving Users
+
+            if (user == null)
+                return Unauthorized(new { message = "Invalid email or password" });
+
+            if (user.PasswordHash != request.Password)
+                return Unauthorized(new { message = "Invalid Password" });
+
+            return Ok(new
+            {
+                user.Id,
+                user.Name,
+                user.Email,
+                user.Role 
+
+            });
+
+            
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using onboardingAPI.Models;
+
+
+namespace onboardingAPI.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+        public DbSet<User> Users { get; set; }
+    
+    }
+}
